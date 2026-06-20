b009cc065fd02192f5c7e59c175e5c8af70996af
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-06-20 13:28:15 -0400

    Crear contrato para PerfilLector

---
+++ SIGEBI.Application/Interfaces/Repositories/ReadOnly.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIGEBI.Application.Interfaces.Repositories
+{
+    public interface ReadOnly<T> where T : class
+    {
+        Task<T?> ObtenerPorIdAsync(object id);
+        Task<IEnumerable<T>> ObtenerTodosAsync();
+    }
+}
