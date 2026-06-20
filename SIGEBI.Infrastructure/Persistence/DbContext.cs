0299f138e0bb380bdcf756b4e5afb4d39313dff5
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-06-20 01:05:30 -0400

    Creacion DbContext

--- SIGEBI.Infrastructure/Persistence/DbContext.cs
+++ SIGEBI.Infrastructure/Persistence/DbContext.cs
@@ -1,8 +1,25 @@
 ﻿
+using SIGEBI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace SIGEBI.Infrastructure.Persistencia
 {
-    internal class DbContext
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
     {
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<PerfilLector> PerfilesLectores { get; set; }
+        public DbSet<Prestamo> Prestamos { get; set; }
+
+        public DbSet<RecursoBibliografico> RecursoBibliograficos { get; set; }
+        public DbSet<Penalizacion> Penalizaciones { get; set; }
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DbContext).Assembly);
+        }
     }
 }
