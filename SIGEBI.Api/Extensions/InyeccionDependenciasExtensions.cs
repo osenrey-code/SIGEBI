e3e00205ab39707df9e4de00806f46de9e629621
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-06-30 09:45:59 -0400

    registrar casos de uso en DI

--- SIGEBI.Api/Extensions/InyeccionDependenciasExtensions.cs
+++ SIGEBI.Api/Extensions/InyeccionDependenciasExtensions.cs
@@ -1,8 +1,15 @@
-﻿using SIGEBI.Application.UseCase.Devoluciones;
-using SIGEBI.Application.UseCase.Prestamos;
-using SIGEBI.Application.UseCase.Usuarios;
-using SIGEBI.Application.Interfaces.ext;
+﻿using SIGEBI.Application.Interfaces.ext;
 using SIGEBI.Application.Services;
+
+using SIGEBI.Application.UseCase.Usuarios;
+using SIGEBI.Application.UseCase.Prestamos;
+using SIGEBI.Application.UseCase.Devoluciones;
+using SIGEBI.Application.UseCase.Catalogo;
+using SIGEBI.Application.UseCase.Penalizaciones;
+using SIGEBI.Application.UseCase.Notificaciones;
+using SIGEBI.Application.UseCase.Auditoria;
+using SIGEBI.Application.UseCase.Reportes;
+
 namespace SIGEBI.Api.Extensions
 {
     public static class InyeccionDependenciasExtensions
@@ -11,6 +18,7 @@ namespace SIGEBI.Api.Extensions
             this IServiceCollection services)
         {
             services.AddScoped<INotificador, Notificador>();
+
             // Usuarios
             services.AddScoped<RegistrarUsuario>();
             services.AddScoped<RegistrarUsuarioWeb>();
@@ -19,6 +27,14 @@ namespace SIGEBI.Api.Extensions
             services.AddScoped<ConsultarUsuarios>();
             services.AddScoped<AsignarPerfilLector>();
             services.AddScoped<AutenticarUsuario>();
+
+            // Catálogo / Recursos
+            services.AddScoped<RegistrarRecurso>();
+            services.AddScoped<ActualizarRecurso>();
+            services.AddScoped<CambiarEstadoRecurso>();
+            services.AddScoped<ConsultarCatalogo>();
+            services.AddScoped<ConsultarDetalleRecurso>();
+            services.AddScoped<ConsultarHistorialRecurso>();
 
             // Préstamos
             services.AddScoped<SolicitarPrestamo>();
@@ -29,8 +45,29 @@ namespace SIGEBI.Api.Extensions
             services.AddScoped<ConsultarHistorialPrestamos>();
             services.AddScoped<RegistrarPrestamoPresencial>();
 
+      
             // Devoluciones
             services.AddScoped<RegistrarDevoluciones>();
+            services.AddScoped<ConsultarHistorialDevoluciones>();
+
+            // Penalizaciones
+            services.AddScoped<ConsultarPenalizaciones>();
+            services.AddScoped<ConsultarPenalizacionesActivas>();
+            services.AddScoped<ResolverPenalizacion>();
+
+            // Notificaciones
+            services.AddScoped<ConsultarNotificaciones>();
+            services.AddScoped<EnviarRecordatorioVencimiento>();
+
+
+            // Auditoría
+            services.AddScoped<ConsultarLogAuditoria>();
+
+            // Reportes
+            services.AddScoped<GenerarReportePrestamo>();
+            services.AddScoped<GenerarReportePenalizaciones>();
+            services.AddScoped<GenerarReporteInventario>();
+            services.AddScoped<GenerarReportesUsoCatalogo>();
 
             return services;
         }
