9f39df249c086009fa6f320217d340936c832f86
Osvaldo Enrrique Reynoso Corona <osenrey@gmail.com>
2026-07-02 22:16:07 -0400

    Ajusta infrastructure de gestion de recursos

---
+++ SIGEBI.Infrastructure/Persistence/Configuration/CategoriaConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SIGEBI.Domain.Entities;
+
+namespace SIGEBI.Infrastructure.Persistence.Configuration
+{
+    internal class CategoriaConfiguration : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("Categorias");
+
+            builder.HasKey(c => c.CategoriaId);
+
+            builder.Property(c => c.Nombre)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasIndex(c => c.Nombre)
+                .IsUnique();
+
+            builder.Property(c => c.Descripcion)
+                .HasMaxLength(300);
+        }
+    }
+}
