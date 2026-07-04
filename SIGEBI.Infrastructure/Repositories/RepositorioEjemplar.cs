86ddd63bbdad9b87b85326ceb2f5cf5a7035d6eb
Osvaldo Enrrique Reynoso Corona <osenrey@gmail.com>
2026-07-04 03:45:48 -0400

    Implementar repositorio ejemplar

---
+++ SIGEBI.Infrastructure/Repositories/RepositorioEjemplar.cs
@@ -0,0 +1,25 @@
+﻿using SIGEBI.Application.Interfaces.Repositories;
+using SIGEBI.Domain.Entities;
+using SIGEBI.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace SIGEBI.Infrastructure.Repositories
+{
+    public class RepositorioEjemplar : RepositorioBase<Ejemplar>, IEjemplarRepository
+    {
+
+            public RepositorioEjemplar(SIGEBIDbContext context) : base(context)
+            {
+
+            }
+
+            public async Task<Ejemplar?> ObtenerPorIdAsync(int id)
+            {
+
+                return await _context.Ejemplares
+                    .Include(e => e.RecursoBibliografico)
+                    .FirstOrDefaultAsync(e => e.EjemplarId == id);
+            }
+
+    }
+}
