ffc44219d43ff64f2151e1ed66ffd15230acb1fb
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-07-11 11:39:20 -0400

    Crear DependencyInjection para implementar

---
+++ SIGEBI.Application/Dependency/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SIGEBI.Application.Interfaces;
+using SIGEBI.Application.UseCase;
+using SIGEBI.Application.UseCase.Catalogo;
+using SIGEBI.Application.UseCase.Prestamos;
+using SIGEBI.Application.UseCase.Usuarios;
+
+namespace SIGEBI.Application.Dependency
+{
+    public static class DependencyInjection 
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+           
+
+        }
+    }
+}
