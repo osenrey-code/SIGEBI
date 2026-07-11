433eefc4a8f449b3569053ce6026ef8f89748d03
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-07-11 19:41:06 -0400

    Modificar implementacion de servicio de password

--- SIGEBI.Infrastructure/Services/ServicioPassword.cs
+++ SIGEBI.Infrastructure/Services/ServicioPassword.cs
@@ -10,18 +10,24 @@ namespace SIGEBI.Infrastructure.Services
         private const int Iterations = 100000;
 
         private static readonly HashAlgorithmName Algorithm =
-            HashAlgorithmName.SHA3_256;
+            HashAlgorithmName.SHA256;
 
         public string GenerarHash(string password)
         {
             if (string.IsNullOrWhiteSpace(password))
             {
-                throw new ArgumentNullException("La contraseña es obligatoria.");
+                throw new ArgumentNullException(nameof(password), "La contraseña es obligatoria.");
             }
 
             var salt = RandomNumberGenerator.GetBytes(SaltSize);
 
-            var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, Algorithm, KeySize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                Iterations,
+                Algorithm,
+                KeySize
+            );
 
             return $"{Iterations}.{Convert.ToBase64String(salt)}.{Convert.ToBase64String(hash)}";
         }
