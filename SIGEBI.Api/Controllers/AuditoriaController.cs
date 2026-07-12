7129a0ee779d80e5fcc48dcecd3d23c4b690984a
Osvaldo Enrrique Reynoso Corona <osenrey@gmail.com>
2026-07-12 17:44:06 -0400

    Implementar controladores de los modulos osvaldo

---
+++ SIGEBI.Api/Controllers/AuditoriaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIGEBI.Application.DTOs.Request;
+using SIGEBI.Application.UseCase.Auditory;
+
+namespace SIGEBI.Api.Controllers
+{
+    [ApiController]
+    [Route("api/auditoria")]
+    public class AuditoriaController : ControllerBase
+    {
+        private readonly ConsultarLogAuditoria _consultarLogAuditoria;
+
+        public AuditoriaController(
+            ConsultarLogAuditoria consultarLogAuditoria)
+        {
+            _consultarLogAuditoria = consultarLogAuditoria;
+        }
+
+        // GET: api/auditoria
+        // GET: api/auditoria?usuarioId=2
+        // GET: api/auditoria?accion=Registrar Usuario
+        // GET: api/auditoria?entidadAfectada=Usuarios
+        [HttpGet]
+        public async Task<IActionResult> ConsultarRegistros(
+            [FromQuery] ConsultarLogAuditoriaRequest request)
+        {
+            int usuarioEjecutorId = 1;
+
+            var registros = await _consultarLogAuditoria
+                .EjecutarAsync(
+                    request,
+                    usuarioEjecutorId
+                );
+
+            return Ok(registros);
+        }
+    }
+}
---
+++ SIGEBI.Api/Controllers/CategoriaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SIGEBI.Application.DTOs.Request;
+using SIGEBI.Application.Interfaces.Service;
+
+namespace SIGEBI.Api.Controllers
+{
+    [ApiController]
+    [Route("api/categorias")]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly IGestionCategorias _gestionCategorias;
+
+        public CategoriasController(IGestionCategorias gestionCategorias)
+        {
+            _gestionCategorias = gestionCategorias;
+        }
+
+        // POST: api/categorias/registrar
+        [HttpPost("registrar")]
+        public async Task<IActionResult> RegistrarCategoria(
+            [FromBody] CategoriaRequest request)
+        {
+            int actorId = 1;
+
+            var categoria = await _gestionCategorias
+                .RegistrarCategoriaAsync(request, actorId);
+
+            return Ok(new
+            {
+                Mensaje = "Categoría registrada correctamente.",
+                Categoria = categoria
+            });
+        }
+
+        // GET: api/categorias
+        [HttpGet]
+        public async Task<IActionResult> ConsultarCategorias()
+        {
+            var categorias = await _gestionCategorias
+                .ConsultarCategoriasAsync();
+
+            return Ok(categorias);
+        }
+    }
+}
