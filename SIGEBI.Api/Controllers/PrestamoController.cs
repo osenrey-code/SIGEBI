2314c74b9f591924fa18e6ca198f0bb860784a22
enmanuel19diaz-rgb <enmanuel19diaz@gmail.com>
2026-07-12 00:50:37 -0400

    Desarrollar Controlador

--- SIGEBI.Api/Controllers/PrestamoController.cs
+++ SIGEBI.Api/Controllers/PrestamoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using SIGEBI.Application.Interfaces.Service;
+using SIGEBI.Application.DTOs.Request;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SIGEBI.Api.Controllers
 {
@@ -6,10 +8,41 @@ namespace SIGEBI.Api.Controllers
     [Route("api/[controller]")]
     public class PrestamoController : ControllerBase
     {
-        [HttpGet("estado")]
-        public IActionResult GetResultado()
+        private readonly IGestionPrestamos _prestamos;
+
+        public PrestamoController(IGestionPrestamos prestamos)
         {
-            return Ok(new { mensaje = "!EL controlador de Préstamo esta conectado a SIGEBI." });
+            _prestamos = prestamos;
+        }
+
+        [HttpPost("solicitarPrestamo")]
+        public async Task<IActionResult> SolicitarPrestamo([FromBody] RegistrarSolicitudRequest request)
+        {
+            int usuarioId = 2;
+            await _prestamos.SolicitarPrestamoAsync(request, usuarioId);
+            return Ok(new { Mensaje = "Solictud realizada correctamente." });
+        }
+
+        [HttpPost("aprobarPrestamo")]
+        public async Task<IActionResult> AprobarPrestamo([FromBody] AprobarSolicitudRequest request)
+        {
+            int usuarioId = 1;
+            await _prestamos.AprobarPrestamoAsync(request, usuarioId);
+            return Ok(new { Mensaje = "Prestamo aprobado de manera exitosa." });
+        }
+
+        [HttpGet("historialPrestamos")]
+        public async Task<IActionResult> HistorialPrestamo([FromQuery] ConsultarHistorialPrestamosRequest request)
+        {
+            var historial = await _prestamos.ConsultarHistorialAsync(request);
+            return Ok(historial);
+        }
+
+        [HttpGet("consultarActivos")]
+        public async Task<IActionResult> ConsultarActivos([FromQuery] ConsultarPrestamosActivosRequest request)
+        {
+            var activos = await _prestamos.ConsultarPrestamosActivosAsync(request);
+            return Ok(activos);
         }
     }
 }
